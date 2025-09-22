ded79464c743055605c11e869eb8a1d1aa993878
gustavopaul <43386193+gustavopaul@users.noreply.github.com>
2025-09-21 22:03:37 -0300

    Saludo nombre formulario

---
+++ codigo.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo!!!"); // prueba consola
+
+function mostrar() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    alert("Hola " + nombre + " " + apellido);
+}
+
